8332f400fb0544345a65d3856d058cad4f47f761
Martin Ivanov <whiteshadow.code@gmail.com>
2013-04-07 19:42:18 +0300

    Added calendar to boilerplate
    calander stuff

--- FMICalendar/h5bp-html5-boilerplate-3cd1967/js/main.js
+++ FMICalendar/h5bp-html5-boilerplate-3cd1967/js/main.js
@@ -1,1 +1,11 @@
+window.onload = function(e){ 
+    var obj = $(this);
+	removeOtherWeeks(obj);
+}
 
+function removeOtherWeeks(obj){
+            var today = new Date();
+            today = formatDate(today.getFullYear(), today.getMonth(), today.getDate());
+            $("#"+today, obj).parentNode.addClass("today");
+        };
+		
